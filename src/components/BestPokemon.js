eb1cf6f5df59925a1f80bac3a4d3fb5daa761a3a
Jeff Zinger <jzinger100@gmail.com>
2020-06-17 20:58:02 +0200

    Fetch pokemon names

--- src/components/BestPokemon.js
+++ src/components/BestPokemon.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function BestPokemon(props) {
-    return <div>
-    <ul>
-    {props.abilities.map((name, index) => {
-      return (
-        <li key={index}>{name}</li>
-      )
-    })}
-  </ul>
+class BestPokemon extends Component {
+  state = {
+    pokemonNames: [],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokedex/1/")
+      .then(res => res.json())
+      .then(data => {
+        const names = [data.pokemon_entries[0].pokemon_species.name, data.pokemon_entries[3].pokemon_species.name, data.pokemon_entries[6].pokemon_species.name]
+        setTimeout(() => {
+          this.setState({
+            pokemonNames: names,
+            isLoading: false
+          })
+        }, 1000)
+      });
+  }
+
+  render() {
+    return (
+    <div>
+      {this.state.isLoading ? <span>Loading...</span> :
+      <>
+        <h3>Names</h3>
+        <ul>
+          {this.state.pokemonNames.map((name, index) => {
+            return (
+              <li key={index}>{name}</li>
+            )
+          })}
+        </ul>
+        <h3>Abilities</h3>
+        <ul>
+          {this.props.abilities.map((name, index) => {
+            return (
+              <li key={index}>{name}</li>
+            )
+          })}
+        </ul>
+      </>
+      }
+      
     </div>
+    )
+  }
 }
 
 export default BestPokemon;
