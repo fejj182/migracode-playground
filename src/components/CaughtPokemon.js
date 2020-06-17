f2daada8e72c812454f8e8650d4e30ca02a1d999
Jeff Zinger <jzinger100@gmail.com>
2020-06-17 19:39:42 +0200

    State added to CaughtPokemon

--- src/components/CaughtPokemon.js
+++ src/components/CaughtPokemon.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function CaughtPokemon(props) {
-    return <div>
-    <p>Caught 0 Pokemon on </p>{props.date}
-    </div>
+class CaughtPokemon extends Component {
+    state = {
+        caughtPokemon: 0
+    }
+
+    catch = () => {
+        this.setState(prevState => {
+            return {
+                caughtPokemon: prevState.caughtPokemon + 1
+            }
+        })
+    }
+
+    reset = () => {
+        this.setState(() => {
+            return {
+                caughtPokemon: 0
+            }
+        })
+    }
+    
+    render() {
+        return (
+        <div>
+            <p>Caught {this.state.caughtPokemon} Pokemon on </p>{this.props.date}
+            <button onClick={this.catch}>Catch</button>
+
+            {this.state.caughtPokemon > 0 
+            ? <ResetButton reset={this.reset}/> 
+            : null
+            }
+            
+        </div>
+        )
+    }
+}
+
+const ResetButton = (props) => {
+    return (
+        <button onClick={props.reset}>Reset</button>
+    )
 }
 
 export default CaughtPokemon;
